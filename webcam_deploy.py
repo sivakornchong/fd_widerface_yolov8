9d1d479d4fabebd04781a1224fd796b753a3227a
Sivakorn Chong <sivakornchong@gmail.com>
2024-02-19 19:11:56 -0800

    Done with prototype and webcam deployment

--- webcam_deploy.py
+++ webcam_deploy.py
@@ -23,7 +23,6 @@ def calcage(input_image, model):
     # Define the transformation
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     # Apply the transformation
@@ -34,7 +33,8 @@ def calcage(input_image, model):
         outputs = model(input_batch)
         y_hat = nn.functional.softmax(outputs, dim=1)  # This is to calculate the losses. 
         y_hat_labels = torch.argmax(y_hat, dim=1)
-        print("The frame has an image with:", labels[y_hat_labels])
+        # print(y_hat)
+        # print("The frame has an image with:", labels[y_hat_labels])
     return y_hat_labels
 
 # start webcam
@@ -42,13 +42,14 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-# Model
-# model = YOLO("yolo-Weights/yolov8n.pt")
+# Video writer setup
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 # Test with an self-trained face model based on YOLOV8, using face detection dataset
 model = YOLO("model/best.pt")
 
-model_age_load_path = 'model/cls_190224.pt'
+model_age_load_path = 'model/fd_age_cls_190224_2.pt'
 model_age = torch.load(model_age_load_path).to(device)
 model_age.eval()
 
@@ -70,6 +71,7 @@ labels = ['Young Children','Children and Youth','Young Adults', 'Adults', 'Older
 
 save = False
 age = True
+video = True
 expand_ratio = 0.2
 
 # Counter for unique filenames
@@ -100,11 +102,11 @@ while True:
 
             # confidence
             confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
+            # print("Confidence --->",confidence)
 
             # class name
             cls = int(box.cls[0])
-            print("Class name -->", classNames[cls])
+            # print("Class name -->", classNames[cls])
 
             # object details
             org = [x1, y1]
@@ -138,14 +140,18 @@ while True:
                 else:
                     cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
-
+                if video:
+                    # Write the frame to the video output
+                     out.write(img)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
         break
     
-    time.sleep(1)  # Introduce a delay of 1 second
+    time.sleep(0.1)  # Introduce a delay of 1 second
 
+# Release the video writer and capture objects
+out.release()
 cap.release()
 cv2.destroyAllWindows()
 
